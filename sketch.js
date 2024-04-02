abdf73869a218b387a63d02038d0f0937817ffc1
Paxton W <102954952+Paxton-W@users.noreply.github.com>
2024-04-02 01:46:50 +0100

    0402

--- sketch.js
+++ sketch.js
@@ -1,3 +1,9 @@
+//
+//
+// PlayPal (original: game assist)
+//
+//by paxton
+
 let cvs;
 let font1;
 let img_arrow;
@@ -29,6 +35,14 @@ let clr = {
   greyLight: "#777",
 };
 let orientationA = "v";
+
+window.addEventListener("load", function () {
+  // Set a timeout to ensure the address bar is hidden
+  setTimeout(function () {
+    // Hide the address bar by scrolling down 1 pixel
+    window.scrollTo(0, 1);
+  }, 0);
+});
 
 function windowResized() {
   var parentElement = document.getElementById("p5cvs");
@@ -64,9 +78,10 @@ function setup() {
   cvs = createCanvas(10, 10);
   cvs.parent("p5cvs");
   windowResized();
-  function createAButton(vari, lable, xS, yS) {
+  //createAButton(btn name, btn lable, x pos, y pos)
+  function createAButton(vari, lable, xS, yS, size = 1) {
     let buttonVar = createButton(lable);
-    buttonVar.sizeSet = { w: 0.2, h: 0.07, font: 10 };
+    buttonVar.sizeSet = { w: 0.2 * size, h: 0.07 * size, font: size };
     buttonVar.pos = { x: xS, y: yS };
     buttonVar.position(
       buttonVar.pos.x * ww - vw * buttonVar.sizeSet.w * 0.5,
@@ -74,31 +89,38 @@ function setup() {
       "fixed"
     );
     buttonVar.size(vw * buttonVar.sizeSet.w, vw * buttonVar.sizeSet.h);
-    buttonVar.style("font-size", String(vw * 0.02) + "pt");
+    buttonVar.style("font-size", String(vw * 0.02 * buttonVar.sizeSet.font) + "pt");
+    buttonVar.addClass("button");
+    buttonVar.id(vari);
     buttonVar.hide();
+    //p5 mousePressed
     buttonVar.trigger = false;
     buttonVar.mousePressed(() => {
       buttonVar.trigger = true;
-      console.log(buttonVar);
+      // console.log(buttonVar);
     });
-    buttonVar.addClass("button");
-    buttonVar.id(vari);
+    //js eventlistener
+    buttonVar.click = false;
     buttonVar.elt.addEventListener("click", () => {
-      console.log(vari);
+      // console.log(vari);
+      buttonVar.click = true;
     });
     Object.assign(btns, { [vari]: buttonVar });
     console.log(buttonVar);
   }
-
-  createAButton("btn_start", "Start", 0.5, 0.6);
-  createAButton("btn_2p", "2p", 0.5, 0.5);
-  createAButton("btn_3p", "3p", 0.5, 0.6);
-  createAButton("btn_3pc", "3pc", 0.5, 0.7);
-  createAButton("btn_4p", "4p", 0.5, 0.8);
+  //welcome page button
+  createAButton("btn_start", "Start", 0.5, 0.7, 1.5);
+  //function menu
+  createAButton("func_timer", "Timer", 0.5, 0.4, 1.2);
+  //rummikub players amount
+  createAButton("timer_2p", "2p", 0.5, 0.5);
+  createAButton("timer_3p", "3p", 0.5, 0.6);
+  createAButton("timer_3pc", "3p (Corner)", 0.5, 0.7);
+  createAButton("timer_4p", "4p", 0.5, 0.8);
   console.log(btns);
   // console.log({ orientationA });
   gameState = "welcome";
-  // btns.btn_start.show();
+  btns.btn_start.show();
   textFont(font1);
   angleMode(DEGREES);
   imageMode(CENTER);
@@ -107,44 +129,45 @@ function setup() {
   noStroke();
 }
 function mousePressed() {
-  if (gameState == "welcome") {
-    if (mouseX > w * 0.46 && mouseX < w * 0.54) {
-      gameState = "timer_select";
-    }
-  } else if (gameState == "timer_select") {
-    if (mouseX > w * 0.46 && mouseX < w * 0.54 && mouseY > h * 0.4 - vw * 0.05 && mouseY < h * 0.4 + vw * 0.05) {
-      //2p
-      gameplayPerson = 2;
-      gameplay3pCornerMode = false;
-    } else if (mouseX > w * 0.46 && mouseX < w * 0.54 && mouseY > h * 0.5 - vw * 0.05 && mouseY < h * 0.5 + vw * 0.05) {
-      //3p
-      gameplayPerson = 3;
-      gameplay3pCornerMode = false;
-    } else if (mouseX > w * 0.46 && mouseX < w * 0.54 && mouseY > h * 0.6 - vw * 0.05 && mouseY < h * 0.6 + vw * 0.05) {
-      //3pc
-      gameplayPerson = 3;
-      gameplay3pCornerMode = true;
-    } else if (mouseX > w * 0.46 && mouseX < w * 0.54 && mouseY > h * 0.7 - vw * 0.05 && mouseY < h * 0.7 + vw * 0.05) {
-      //4p
-      gameplayPerson = 4;
-      gameplay3pCornerMode = false;
-    }
-    gameplayPersonTurns = 0;
-    gameState = "play";
-    t_cur_timer = t_set_timer;
-    console.log({ gameplayPerson, gameplayPersonTurns, gameplay3pCornerMode });
-  } else if (gameState == "play") {
-    if (mouseX > vw * 0 && mouseX < vw * 0.15 && mouseY > h * 0 && mouseY < vw * 0.15) {
-      gameState = "pause";
-    } else if (millis() - lastChangeTouch > 500) {
-      touch_next_trigger = true;
-      s_pop.play();
-      console.log(t_cur_timer);
-      lastChangeTouch = millis();
-    }
-  }
+  // if (gameState == "welcome") {
+  //   if (mouseX > w * 0.46 && mouseX < w * 0.54) {
+  //     gameState = "timer_select";
+  //   }
+  // } else if (gameState == "timer_select") {
+  //   if (mouseX > w * 0.46 && mouseX < w * 0.54 && mouseY > h * 0.4 - vw * 0.05 && mouseY < h * 0.4 + vw * 0.05) {
+  //     //2p
+  //     gameplayPerson = 2;
+  //     gameplay3pCornerMode = false;
+  //   } else if (mouseX > w * 0.46 && mouseX < w * 0.54 && mouseY > h * 0.5 - vw * 0.05 && mouseY < h * 0.5 + vw * 0.05) {
+  //     //3p
+  //     gameplayPerson = 3;
+  //     gameplay3pCornerMode = false;
+  //   } else if (mouseX > w * 0.46 && mouseX < w * 0.54 && mouseY > h * 0.6 - vw * 0.05 && mouseY < h * 0.6 + vw * 0.05) {
+  //     //3pc
+  //     gameplayPerson = 3;
+  //     gameplay3pCornerMode = true;
+  //   } else if (mouseX > w * 0.46 && mouseX < w * 0.54 && mouseY > h * 0.7 - vw * 0.05 && mouseY < h * 0.7 + vw * 0.05) {
+  //     //4p
+  //     gameplayPerson = 4;
+  //     gameplay3pCornerMode = false;
+  //   }
+  //   gameplayPersonTurns = 0;
+  //   gameState = "play";
+  //   t_cur_timer = t_set_timer;
+  //   console.log({ gameplayPerson, gameplayPersonTurns, gameplay3pCornerMode });
+  // } else if (gameState == "play") {
+  //   if (mouseX > vw * 0 && mouseX < vw * 0.15 && mouseY > h * 0 && mouseY < vw * 0.15) {
+  //     gameState = "pause";
+  //   } else if (millis() - lastChangeTouch > 500) {
+  //     touch_next_trigger = true;
+  //     s_pop.play();
+  //     console.log(t_cur_timer);
+  //     lastChangeTouch = millis();
+  //   }
+  // }
 }
 function draw() {
+  btnPress();
   Object.values(btns).forEach((b) => {
     if (b.trigger) {
       console.log("1");
@@ -153,6 +176,7 @@ function draw() {
   });
   background(clr.greyDark);
   fill("#222");
+  rect(w / 2, h / 2, vw * 0.8, vh * 0.8, 10);
   if (orientationA == "h") {
     push();
     rect(w / 2, h / 2, vw * 0.8, vh * 0.8, 10);
@@ -166,18 +190,24 @@ function draw() {
   }
   if (gameState == "welcome") {
     push();
-    rect(w / 2, h / 2, vw * 0.8, vh * 0.8, 10);
     fill(clr.white);
     textSize(vw * 0.1);
     text("Gamer Assist", w / 2, h * 0.4);
     textSize(vw * 0.04);
     text("Timer", w / 2, h * 0.48);
-    fill(clr.red);
-    translate(w / 2, h * 0.6);
-    rect(0, 0, vw * 0.4, vw * 0.1, 10);
-    textSize(vw * 0.06);
-    fill(255);
-    text("Start", 0, 0);
+    // fill(clr.red);
+    // translate(w / 2, h * 0.6);
+    // rect(0, 0, vw * 0.4, vw * 0.1, 10);
+    // textSize(vw * 0.06);
+    // fill(255);
+    // text("Start", 0, 0);
+    pop();
+  } else if (gameState == "func_select") {
+    push();
+    fill(clr.white);
+    textSize(vw * 0.1);
+    text("Select Tool", w / 2, h * 0.2);
+    textSize(vw * 0.04);
     pop();
   } else if (gameState == "timer_select") {
     push();
@@ -185,42 +215,6 @@ function draw() {
     fill(255);
     textSize(vw * 0.1);
     text("Select", w / 2, h * 0.3);
-    pop();
-    //2p
-    push();
-    fill(160, 16, 15);
-    translate(w / 2, h * 0.4);
-    rect(0, 0, vw * 0.4, vw * 0.1, 10);
-    textSize(vw * 0.06);
-    fill(255);
-    text("2p", 0, 0);
-    pop();
-    //3p
-    push();
-    fill(160, 16, 15);
-    translate(w / 2, h * 0.5);
-    rect(0, 0, vw * 0.4, vw * 0.1, 10);
-    textSize(vw * 0.06);
-    fill(255);
-    text("3p", 0, 0);
-    pop();
-    //3p corner
-    push();
-    fill(160, 16, 15);
-    translate(w / 2, h * 0.6);
-    rect(0, 0, vw * 0.4, vw * 0.1, 10);
-    textSize(vw * 0.06);
-    fill(255);
-    text("3p Corner", 0, 0);
-    pop();
-    //4p
-    push();
-    fill(160, 16, 15);
-    translate(w / 2, h * 0.7);
-    rect(0, 0, vw * 0.4, vw * 0.1, 10);
-    textSize(vw * 0.06);
-    fill(255);
-    text("4p", 0, 0);
     pop();
   } else if (gameState == "play") {
     //arrow
@@ -312,5 +306,56 @@ function draw() {
       text(`0${t_min}:${t_sec}`, 0, 0);
     }
     pop();
+  }
+}
+
+function btnPress() {
+  if (gameState == "welcome" && btns.btn_start.click) {
+    gameState = "func_select";
+    btns.btn_start.click = false;
+    btns.btn_start.hide();
+    //display function select button
+    btns.func_timer.show();
+  } else if (gameState == "func_select" && btns.func_timer.click) {
+    gameState = "timer_select";
+    btns.func_timer.click = false;
+    btns.func_timer.hide();
+    //show timer player amount select button
+    btns.timer_2p.show();
+    btns.timer_3p.show();
+    btns.timer_3pc.show();
+    btns.timer_4p.show();
+  } else if (gameState == "timer_select") {
+    function gameSetup_37849() {
+      gameplayPersonTurns = 0;
+      gameState = "play";
+      t_cur_timer = t_set_timer;
+      btns.timer_2p.hide();
+      btns.timer_3p.hide();
+      btns.timer_3pc.hide();
+      btns.timer_4p.hide();
+    }
+    if (btns.timer_2p.click) {
+      gameplayPerson = 2;
+      gameplay3pCornerMode = false;
+      gameSetup_37849();
+      btns.timer_2p.click = false;
+    } else if (btns.timer_3p.click) {
+      gameplayPerson = 3;
+      gameplay3pCornerMode = false;
+      gameSetup_37849();
+      btns.timer_3p.click = false;
+    } else if (btns.timer_3pc.click) {
+      gameplayPerson = 3;
+      gameplay3pCornerMode = true;
+      gameSetup_37849();
+      btns.timer_3pc.click = false;
+    } else if (btns.timer_4p.click) {
+      gameplayPerson = 4;
+      gameplay3pCornerMode = false;
+      gameSetup_37849();
+      btns.timer_4p.click = false;
+    }
+  } else if (gameState == "play") {
   }
 }
